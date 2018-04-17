f3cf95fb48d794841ce490b5ca2db7990d886ca6
Michał Urban <michalurban23@gmail.com>
2018-04-17 12:55:05 +0200

    Refactor App

--- js/App.js
+++ js/App.js
@@ -5,22 +5,6 @@ function addListeners(controller) {
     controller.readText();
 }
 
-function keywordController(keywords) {
-
-    let suggestions = [];
-    let input = readText();
-
-    for (let keyword of keywords) {
-        if (keyword.contains(input)) {
-            suggestions.push(keyword);
-        }
-    }
-
-    console.log(suggestions.toString())
-
-}
-
-
 $("document").ready(function() {
     let controller = new Controller();
     let dictionary = new Dictionary();
--- js/Controller.js
+++ js/Controller.js
@@ -4,4 +4,18 @@ export class Controller {
             console.log($(this)[0].value);
         });
     }
+
+    keywordController(keywords) {
+
+        let suggestions = [];
+        let input = this.readText();
+
+        for (let keyword of keywords) {
+            if (keyword.contains(input)) {
+                suggestions.push(keyword);
+            }
+        }
+
+        console.log(suggestions.toString())
+    }
 }
