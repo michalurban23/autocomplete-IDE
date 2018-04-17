3fb41c8347cc528829d0c5b8983fc69abf44a40d
Kacper Zelichowski <kafejek@gmail.com>
2018-04-17 10:27:12 +0200

    Loadkeywords method implemented

--- js/App.js
+++ js/App.js
@@ -2,6 +2,21 @@ function createEditorContainer() {
     return "<div>dupa</div>";
 }
 
+
+function loadKeywords() {
+
+    var keywords = [];
+
+    $.get("assets/keywords.txt", function (data) {
+        var lines = data.split('\n');
+        $.forEach(lines, function (line) {
+            keywords.push(line)
+            console.log(line)
+        })
+    })
+}
+
 $("document").ready(function() {
     $("#app").append(createEditorContainer);
+    loadKeywords()
 });
