24ab2def84f486c2bc10258aa5c0a73d02f3825a
Ernest <ernestjaroszek22@gmail.com>
2018-04-18 13:09:26 +0200

    added tab button functionality

--- js/Controller.js
+++ js/Controller.js
@@ -11,6 +11,26 @@ export class Controller {
 
         let self = this;
 
+        $("#code").keydown(function(e) {
+            if(e.keyCode === 9) {
+
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+
+                var $this = $(this);
+                var value = $this.val();
+
+
+                $this.val(value.substring(0, start)
+                    + "\t"
+                    + value.substring(end));
+
+                this.selectionStart = this.selectionEnd = start + 1;
+
+                e.preventDefault();
+            }
+        });
+
         $("#code").on("input", function () {
             console.log($(this)[0]);
             self.giveSuggestions(dict, $(this)[0].value);
